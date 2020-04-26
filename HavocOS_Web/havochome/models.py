ce56ca6f1d5d6dc2c188405b0ce413f2f6c2f4f5
Arna <arnamaity6230@gmail.com>
2020-04-26 17:07:43 +0530

    Updated downloads page.

--- HavocOS_Web/havochome/models.py
+++ HavocOS_Web/havochome/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import requests,json
 
 class Developer(models.Model):
     Name = models.CharField(max_length=100)
-    Role = models.CharField(max_length=100)
+    Role = models.CharField(max_length=100,null=True)
     Desc = models.TextField()
-    Gitlink = models.CharField(max_length=500)
+    Imlink = models.CharField(max_length=500,default="{{ static 'havochome/assets/profile.jpeg' }}")
+    Xdalink = models.CharField(max_length=500,blank=True)
+    Gitlink = models.CharField(max_length=500, blank=True)
     Paylink = models.CharField(max_length=500, blank=True)
     Tellink = models.CharField(max_length=100, blank=True)
 
@@ -13,11 +16,32 @@ class Developer(models.Model):
 
 
 class Device(models.Model):
-    Name = models.CharField(max_length=150)
     Codename = models.CharField(max_length=100)
     Maintainer = models.ForeignKey(Developer, on_delete=models.CASCADE)
-    Dlink = models.CharField(max_length=500)
+    link = models.CharField(max_length=500,null=True)
+    Xdalink = models.CharField(max_length=500,null=True)
+    Sflink = models.CharField(max_length=500,blank=True)
 
 
     def __str__(self):
         return self.Codename
+
+    
+
+
+
+## Anushek Prasal
+#response = requests.get('https://api.github.com/users/skulshady')
+#dev = response.json()
+#Developer(Name=dev['name'],Role=dev['bio'],Desc='',Imlink=dev['avatar_url'],Xdalink='https://forum.xda-developers.com/member.php?u=7578917',Gitlink=dev['html_url'],Paylink='https://paypal.me/ANUSHEK',Tellink='').save()
+#
+## Viktor Hermann
+#response = requests.get('https://api.github.com/users/zenixxx')
+#dev = response.json()
+#Developer(Name=dev['name'],Role=dev['bio'],Desc='',Imlink=dev['avatar_url'],Xdalink='https://forum.xda-developers.com/member.php?u=4608158',Gitlink=dev['html_url'],Paylink='https://paypal.me/vhermann ',Tellink='').save()
+#
+## Arna Maity
+#response = requests.get('https://api.github.com/users/arna-maity')
+#dev = response.json()
+#Developer(Name=dev['name'],Role=dev['bio'],Desc='',Imlink=dev['avatar_url'],Xdalink='',Gitlink=dev['html_url'],Paylink='',Tellink='').save()
+
--- HavocOS_Web/havochome/urls.py
+++ HavocOS_Web/havochome/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='havoc_home'),
     path('about/', views.about, name='havoc_about'),
-    path('downloads/', views.downloads, name='havoc_downloads')
+    path('downloads/', views.downloads, name='havoc_downloads'),
+    path('downloads/<slug:a_ver>', views.downloads, name='havoc_downloads_ver')
 ]
