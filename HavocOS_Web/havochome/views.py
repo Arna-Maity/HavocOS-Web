6af23ff7428e01e8b94e0687662178eca67ec657
Arna <arnamaity6230@gmail.com>
2020-04-26 17:07:43 +0530

    Updated downloads page.

--- HavocOS_Web/havochome/views.py
+++ HavocOS_Web/havochome/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from havochome.models import Developer, Device
+import requests, json
 
 
 def home(request):
     return render(request, 'havochome/home.html')
 
 def about(request):
-    return render(request, 'havochome/about.html',{'title': 'About'})
+    l = []
+    for i in range(len(Developer.objects.all())): 
+        l.append(Developer.objects.get(id=(i+1)))     
+
+    context = { 'devs':  l}
+    print(context)
+    return render(request, 'havochome/about.html',context)
 
 
 def downloads(request):
-    return render(request, 'havochome/downloads.html', {'title': 'Downloads'})
+    l = []
+    for i in range(len(Device.objects.all())): 
+        tmp = {}
+        dev = json.loads(requests.get(Device.objects.get(id=(i+1)).link).content,encoding='utf-8-sig')
+        tmp['name'] = dev['name']
+        tmp['img'] = dev['image']
+        tmp['links'] = Device.objects.get(id=(i+1))
+        l.append(tmp)  
+
+    context = { 'devices': l}
+    return render(request, 'havochome/downloads.html',context)
