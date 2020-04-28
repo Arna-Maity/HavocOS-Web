d5bef51a332a0f43943959635f87fe782900815c
Arna <arnamaity6230@gmail.com>
2020-04-28 13:00:42 +0530

    Updated project.

--- HavocOS_Web/havochome/urls.py
+++ HavocOS_Web/havochome/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='havoc_home'),
     path('about/', views.about, name='havoc_about'),
     path('downloads/', views.downloads, name='havoc_downloads'),
-    path('downloads/<slug:a_ver>', views.downloads, name='havoc_downloads_ver')
+    path('downloads/<int:a_ver>', views.downloads_ver, name='havoc_downloads_ver'),
+    path('vendor/<slug:vendor>',views.device,name='havoc_vendor')
 ]
