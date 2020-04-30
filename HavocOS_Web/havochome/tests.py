1c840fba9a2376e009089258a1c41d4e806fcd21
Arna <arnamaity6230@gmail.com>
2020-04-30 12:23:43 +0530

    Added basic test and updated travis config.

--- HavocOS_Web/havochome/tests.py
+++ HavocOS_Web/havochome/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from havochome.models import Developer, Device
+import requests, json
 
 # Create your tests here.
+
+class DeviceCase(TestCase):
+    def testDevice(self):
+        device = Device(Codename="oneplus3", Maintainer=Developer(Name="Anushek"), link="https://raw.githubusercontent.com/Arna-Maity/havoc-supported-devices/master/info/oneplus3.json")
+        self.assertEqual(device.Codename, "oneplus3")
+        self.assertEqual(device.Maintainer.Name, "Anushek")
+        self.assertEqual(device.link, "https://raw.githubusercontent.com/Arna-Maity/havoc-supported-devices/master/info/oneplus3.json")
+
