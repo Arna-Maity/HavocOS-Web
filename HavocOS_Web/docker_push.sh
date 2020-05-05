40c6c30180a19b2620dfab47d86504ab9d240a99
Arna <arnamaity6230@gmail.com>
2020-05-05 18:09:25 +0530

    Test commit 2.

--- HavocOS_Web/docker_push.sh
+++ HavocOS_Web/docker_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build ./HavocOS_Web/ -t $DOCKER_USERNAME/havocos-web
 docker push $DOCKER_USERNAME/havocos-web
