311d516778d5651ef47507e6a9125396681f9162
Arna <arnamaity6230@gmail.com>
2020-05-05 18:35:16 +0530

    Updated Docker scripts.

--- docker_push.py
+++ docker_push.py
@@ -3,10 +3,15 @@ import os
 DOCKER_USERNAME = os.environ['DOCKER_USERNAME']
 DOCKER_PASSWORD = os.environ['DOCKER_PASSWORD']
 
-login = "echo \"" + DOCKER_PASSWORD = "\" | docker login -u "$DOCKER_USERNAME" --password-stdin
-img = 'docker build ./HavocOS_Web/ -t ' + DOCKER_USERNAME + '/havocos'
-push = 'docker push ' + DOCKER_USERNAME + '/havocos'
+login = "echo \"" + DOCKER_PASSWORD + "\" | docker login -u \"" + DOCKER_USERNAME + "\" --password-stdin"
+img = 'docker build ./HavocOS_Web/ -t ' + DOCKER_USERNAME + '/havocos-web'
+push = 'docker push ' + DOCKER_USERNAME + '/havocos-web'
+print(login)
+print("Logging into Docker ...")
+os.system(login)
 print(img) 
+print("Building Docker image ...")
 os.system(img)
 print(push)
+print("Pushing Docker image to dockerHUB ...")
 os.system(push)
